1ab155cb418f33dc8dea95debcd89c2d0154f952
Florian Stancioiu <florinvictorstancioiu@gmail.com>
2023-03-24 13:44:25 +0200

    Make the slider functional

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import classes from './Slider.module.css';
 import ProductOneImg from '../images/image-product-1.jpg';
 import ProductTwoImg from '../images/image-product-2.jpg';
@@ -6,45 +8,88 @@ import ProductFourImg from '../images/image-product-4.jpg';
 import IconNext from '../images/icon-next.svg';
 import IconPrevious from '../images/icon-previous.svg';
 
-const images = [
-  {
-    id: 1,
-    url: ProductOneImg,
-    active: true,
-  },
-  {
-    id: 2,
-    url: ProductTwoImg,
-    active: false,
-  },
-  {
-    id: 3,
-    url: ProductThreeImg,
-    active: false,
-  },
-  {
-    id: 4,
-    url: ProductFourImg,
-    active: false,
-  },
-];
-
 const Slider = () => {
+  const [images, setImages] = useState([
+    {
+      id: 1,
+      url: ProductOneImg,
+      active: true,
+    },
+    {
+      id: 2,
+      url: ProductTwoImg,
+      active: false,
+    },
+    {
+      id: 3,
+      url: ProductThreeImg,
+      active: false,
+    },
+    {
+      id: 4,
+      url: ProductFourImg,
+      active: false,
+    },
+  ]);
+  const [index, setIndex] = useState(0);
+
+  //
+  const goLeft = () => {
+    if (index === 0) {
+      setIndex(images.length - 1);
+    } else {
+      setIndex((prev) => --prev);
+    }
+
+    updateImages();
+  };
+
+  const goRight = () => {
+    if (index === images.length - 1) {
+      setIndex(0);
+    } else {
+      setIndex((prev) => ++prev);
+    }
+
+    updateImages();
+  };
+
+  const updateImages = () => {
+    setImages((prevImages) => {
+      return prevImages.map((item, imagesIndex2) => {
+        if (imagesIndex2 === index) {
+          return {
+            ...item,
+            active: true,
+          };
+        }
+
+        return {
+          ...item,
+          active: false,
+        };
+      });
+    });
+  };
+
   return (
     <div className={classes.slider}>
       <div className={classes['slider-images']}>
-        {images.map((item, index) => {
-          const imgClasses = item.active
-            ? `${classes['slider-image']} ${classes.active}`
-            : classes['slider-image'];
+        {images.map((item, imagesIndex) => {
+          const imgClasses =
+            imagesIndex === index
+              ? `${classes['slider-image']} ${classes.active}`
+              : classes['slider-image'];
 
-          return <img key={index} src={item.url} className={imgClasses} />;
+          return (
+            <img key={imagesIndex} src={item.url} className={imgClasses} />
+          );
         })}
       </div>
-      <div className={classes['slider-left-btn']}>
+      <div onClick={goLeft} className={classes['slider-left-btn']}>
         <img src={IconPrevious} alt='' />
       </div>
-      <div className={classes['slider-right-btn']}>
+      <div onClick={goRight} className={classes['slider-right-btn']}>
         <img src={IconNext} alt='' />
       </div>
     </div>
